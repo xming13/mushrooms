eb3d09228e1203a380e3352102efb8f566f33ee0
xming13 <xiaoming33@gmail.com>
2015-04-11 13:30:42 +0800

    Prevent multiple posting

--- app/js/mushrooms.js
+++ app/js/mushrooms.js
@@ -334,6 +334,7 @@ XMing.GameStateManager = new function() {
             imageUrl: "images/mushroom-small.png",
             closeOnConfirm: false
         }, function() {
+            var postingInProgress = false;
             swal({
                 title: "Thanks for playing!!!",
                 imageUrl: "images/love.png",
@@ -344,7 +345,11 @@ XMing.GameStateManager = new function() {
                 if (playerName == "") {
                     swal.showInputError("You need to write something! A nickname is fine too!");
                     return false;
+                }
+                if (postingInProgress) {
+                    return false;
                 } else {
+                    postingInProgress = true;
                     $.ajax({
                         method: "POST",
                         url: 'http://weiseng.redairship.com/leaderboard/api/1/highscore.json',
